4d8083353042ce88765842b93575584c180d0407
purging5904 <redmnq77249@gmail.com>
2023-10-13 09:07:33 -0700

    partial implementation

--- client/client.py
+++ client/client.py
@@ -2,27 +2,82 @@
 import socket
 import sys
 import signal
+import threading
+import os
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host_name = socket.gethostname()
 port = 12345
+max_size = 1024
 
 def exit_handler(sig, frame):
-    print("Server is shutting down...")
+    print("Client is shutting down...")
     client_socket.close()
     sys.exit(0)
 
+def receive_msg():
+    while True:
+        data = client_socket.recv(max_size)
+        if not data:
+            print(f'No message from server')
+            break
+        print(f'Received message from server: {data.decode()}')
+
+def send_msg():
+    while True:
+        message = input('Please enter your message to server: ')
+        if message is None or message == 'exit':
+            print('Exiting client')
+            sys.exit(0)
+        client_socket.send(message.encode())
+
+def send_file():
+    while True:
+        path = input("Please input path of the file: ")
+        dirname = os.path.dirname(path)
+        basename = os.path.basename(path)
+        if not dirname and not basename:
+            print('invalid path, please re-input')
+            continue
+        client_socket.send(basename.encode())
+        with open(path, 'rb') as f:
+            while True:
+                line = f.read(max_size)
+                if line:
+                    print('file bytes sent')
+                    client_socket.send(line)
+                else: break
+
+
+
+def menu():
+    print('-------------SOCKET SEND & RECEIVE--------------')
+    print('* Please Choose an option')
+    print('1. SEND/RECEIVE MESSAGE')
+    print('2. SEND/RECEIVE FILE')
+    option = input('* ')
+    option = int(option)
+    if option == 1 or option == 2:
+        return option
+    return None
 
 if __name__ == '__main__':
     client_socket.connect((host_name, port))
-    while True:
-        signal.signal(signal.SIGINT, exit_handler)
-        message = input("Please enter your message sending to server: ")
-        if message is None or message == 'exit':
-            print('Stopping client...')
-            sys.exit(0)
-        client_socket.send(message.encode())
-        data = client_socket.recv(1024)
-        if data is None:
-            print('Received 0 bytes from server')
-        print(f'Received message from server {data.decode()}')
+    signal.signal(signal.SIGINT, exit_handler)
+    op = menu()
+    if op == 1:
+        client_socket.send('message mode'.encode())
+        recv_thread = threading.Thread(target=receive_msg)
+        send_thread = threading.Thread(target=send_msg)
+        recv_thread.start()
+        send_thread.start()
+        recv_thread.join()
+        send_thread.join()
+    elif op == 2:
+        client_socket.send('file mode'.encode())
+        send_file()
+        pass
+    else:
+        pass
+
+
--- server/server.py
+++ server/server.py
@@ -2,10 +2,12 @@
 import socket
 import sys
 import signal
+import threading
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host_name = socket.gethostname()
 port = 12345
+max_size = 1024
 server_socket.bind((host_name, port))
 server_socket.listen(3)
 
@@ -16,18 +18,57 @@ def exit_handler(sig, frame):
     sys.exit(0)
 
 
+def receive(socket):
+    while True:
+        data = client_socket.recv(max_size)
+        if not data:
+            print(f'No message from client')
+            break
+        print(f'Received message from client: {data.decode()}')
+
+def send(socket):
+    while True:
+        message = input('Please enter your message to client: ')
+        if not message or message == 'exit':
+            print('Exiting server')
+            sys.exit(0)
+        client_socket.send(message.encode())
+
+def recv_file(socket):
+    while True:
+        filename = client_socket.recv(max_size).decode()
+        if not filename:
+            continue
+        with open(filename, 'wb') as f:
+            while True:
+                data = socket.recv(max_size)
+                if data:
+                    print('file bytes recv')
+                    f.write(data)
+                else: break
+
+
 
 if __name__ == '__main__':
+    signal.signal(signal.SIGINT, exit_handler)
     print('Server is running and waiting for connections...')
     client_socket, addr = server_socket.accept()
     print(f'Connected Address is {str(addr)}')
-    buffer = 'hello'
-    buffer_max_size = 256
-    while True:
-        signal.signal(signal.SIGINT, exit_handler)
-        data = client_socket.recv(256)
-        if data is None:
-            print('Error while receiving data')
-            sys.exit(0)
-        print(f"Server: received data from client --> {data.decode()}")
-        client_socket.send('Success!'.encode())
+    mode = client_socket.recv(max_size).decode()
+    if mode == 'message mode':
+        recv_thread = threading.Thread(target=receive, args=[client_socket, ])
+        send_thread = threading.Thread(target=send, args=[client_socket, ])
+        recv_thread.start()
+        send_thread.start()
+        recv_thread.join()
+        send_thread.join()
+    elif mode == 'file mode':
+        recv_file(client_socket)
+        pass
+    else:
+        pass
+
+
+
+
+
